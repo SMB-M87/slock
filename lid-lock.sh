a755e6f21f0a0ea14decc4f5f708dd8df96323fc
SMB-M87 <SMB-M87@proton.me>
2025-11-14 23:19:41 +0100

    Suspend lid-lock

--- lid-lock.sh
+++ lid-lock.sh
@@ -9,3 +9,5 @@ XAUTHORITY=/home/$USER/.Xauthority
 logger "Locking screen for $USER"
 /usr/bin/runuser -u "$USER" -- env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY /usr/local/bin/slock &
 sleep 0.5
+
+/usr/bin/loginctl suspend
